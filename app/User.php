a4318d14a09081f3b095cbdb7de9f558c2fcdc62
Edgar Huaranga <edgar.huaranga@uni.pe>
2019-07-31 20:25:40 -0500

    buddy night

--- app/User.php
+++ app/User.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
   public function answers(){
-    return Answer::where('user_id', $this->id);
+    return Answer::where('user_id', $this->id)->get();
   }
 }
