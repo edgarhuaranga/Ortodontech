fc5f9ddd6734fdfb4f3f44dc8f82d7b1b46f680b
Edgar Huaranga <edgar.huaranga@uni.pe>
2019-08-05 11:25:09 -0500

    5 top quizzes

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -241,7 +241,7 @@ class ApiController extends Controller
                   ->join('quizzes', 'quizzes.id', 'questions.quiz_id')
                   ->groupBy('quizzes.topic')
                   ->orderBy('avance','desc')
-                  ->take(10)
+                  ->take(5)
                   ->get();
 
 
