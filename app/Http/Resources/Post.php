343ac204209e8876805e5f016f406213be2ff893
edhu <edgar.huaranga@uni.pe>
2021-05-05 02:34:39 -0500

    br language

--- app/Http/Resources/Post.php
+++ app/Http/Resources/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class Post extends JsonResource
@@ -14,6 +15,7 @@ class Post extends JsonResource
      */
     public function toArray($request)
     {
+      Carbon::setLocale('pt_BR');
       return [
           'id' => $this->id,
           'title' => $this->title,
